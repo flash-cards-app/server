2584ab2a4ab154497c53e4012e7f5b7083a511e1
Reckit075 <mateusz.majka.zsl@gmail.com>
2022-03-27 22:30:49 +0200

    feat: quiz (basic CRUD + update models)

--- models/QuizQuestion.js
+++ models/QuizQuestion.js
@@ -2,21 +2,15 @@ let mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const QuizQuestionSchema = new Schema({
-    question:{
-        type: String,
-    },
+    question:String,
     answers:[String],
-    correctAnswer: {
-        type: Number
-    },
-    quiz:{
+    correctAnswer: Number,
+    quiz:[{
         type: Schema.Types.ObjectId,
         ref:"Quiz"
-    }
-
+    }]
 })
 
 const model = mongoose.model("QuizQuestion", QuizQuestionSchema)
 
 module.exports = {model}
-// module.exports = model
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,29 @@
 let express = require ('express')
 
-let  quiz_get_byType = require('../controlers/quiz.js').quiz_get_byType
+let  {
+    quiz_post_create,
+    quiz_get_byType,
+    quiz_patch_update,
+    quiz_delete_Quiz,
+    quiz_post_Question,
+    quiz_patch_Question,
+    quiz_delete_Question
+} = require('../controlers/quiz.js')
 
 const router = express.Router()
 
+router.post('/create',quiz_post_create)
+
 router.get('/getByType',quiz_get_byType)
+
+router.patch('/quiz', quiz_patch_update)
+
+router.delete('/delete', quiz_delete_Quiz)
+
+router.post('/question',quiz_post_Question)
+
+router.patch('/question', quiz_patch_Question)
+
+router.delete('/question', quiz_delete_Question)
 
 module.exports = {router}
